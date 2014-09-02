22a55fd0242c9f82a46c14b05f26e0f960e175b0
amandakirby <amandakirby1@gmail.com>
2014-09-02 17:41:24 -0400

    getting the basic structure for the site down

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('#navigation').affix({
+      offset: {
+        top: $('.main-background').height()-$('#navigation').height()
+      }
+}); 
+
+$('.scroll-top').affix({
+      offset: {
+        top: $('.main-background').height()-$('#navigation').height()
+      }
+}); 
