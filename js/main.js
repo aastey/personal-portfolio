9970e6bec18abeeb7504da79211df3e680befee2
amandakirby <amandakirby1@gmail.com>
2014-09-02 20:54:23 -0400

    created the rest of the sections for the home page and added in more content

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ $('.scroll-top').affix({
         top: $('.main-background').height()-$('#navigation').height()
       }
 }); 
+
+$( document ).ready( function() {
+  $(".nav-link").click(navChange);
+
+});
+
+var navChange= function(e){
+	$(".nav-link").removeClass("active");
+	$(this).addClass("active");
+   
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 130
+        }, 1000);
+        return false;
+      }
+    }
+
+}
+
