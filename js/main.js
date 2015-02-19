b10920df47fc357223e50b1d7c43b383456850b9
aastey <aastey@gannett.com>
2015-02-19 15:31:47 -0500

    got the navigation highlighting to work and added fade effects to each page load

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-/**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
 
-/**
- * Main AngularJS Web Application
- */
-var app = angular.module('amandaPortfolio', [
-  'ngRoute'
-]);
-/**
- * Configure the Routes
- */
+var app = angular.module('amandaPortfolio', ['ngRoute']);
+
+//Configure the Routes
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-    // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
-    // Pages
     .when("/develop", {templateUrl: "partials/develop.html", controller: "PageCtrl"})
     .when("/design", {templateUrl: "partials/design.html", controller: "PageCtrl"})
     .when("/resume", {templateUrl: "partials/resume.html", controller: "PageCtrl"})
@@ -25,51 +13,47 @@ app.config(['$routeProvider', function ($routeProvider) {
     .otherwise({redirectTo: '/'});
 }]);
 
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+// Controls Main Pages
+app.controller('PageCtrl', function( $scope, $location, $http ) {
 
   // Activates the Carousel
   $('.carousel').carousel({
     interval: 5000
   });
-
-  // Activates Tooltips for Social Links
+   // Activates Tooltips for Social Links
   $('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
-  })
+  });
+
+   $scope.isActive = function(route) {
+        return route === $location.path();
+    }
 });
 
-/**
- * Controls Design Page Template
- */
+
+// Controls Design Page Template
 app.controller("DesignCtrl", function($scope, $http) {
   $http.get('../data/design-projects.json').
     success(function(data, status, headers, config) {
       $scope.posts = data;
     }).
     error(function(data, status, headers, config) {
-      // log error
     });
 });
 
-/**
- * Controls Develop Page Template
- */
+
+// Controls Develop Page Template
 app.controller("DevelopCtrl", function($scope, $http) {
   $http.get('../data/develop-projects.json').
     success(function(data, status, headers, config) {
       $scope.posts = data;
     }).
     error(function(data, status, headers, config) {
-      // log error
     });
 });
 
-/**
- * Controls All Design Sub Pages
- */
+
+// Controls Design Sub Page Template
 app.controller('DesignSubPage', function($scope,$routeParams, $http) {
     $http.get('../data/design-projects.json').
     success(function(data, status, headers, config) {
@@ -88,14 +72,11 @@ app.controller('DesignSubPage', function($scope,$routeParams, $http) {
 
     }).
     error(function(data, status, headers, config) {
-      // log error
     });
 });
 
 
-/**
- * Controls All Develop Sub Pages
- */
+// Controls Develop Page Template
 app.controller('DevelopSubPage', function($scope,$routeParams, $http) {
     $http.get('../data/develop-projects.json').
     success(function(data, status, headers, config) {
@@ -114,10 +95,8 @@ app.controller('DevelopSubPage', function($scope,$routeParams, $http) {
 
     }).
     error(function(data, status, headers, config) {
-      // log error
     });
-
-   
-
 });
+
+
 
